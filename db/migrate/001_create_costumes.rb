6dafb5d035fbaf552beeb416d6fba2d21bcbdb85
Emmanuel <emmanuelamon92@gmail.com>
2021-07-14 11:38:52 -0400

    commit tree ea1980de8b66cee27411110200af3b1b4bb504cb

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -9,3 +9,33 @@
 # value types according to the spec
 
 # !!! Before you run rake db:migrate, remember to fill out the other migration files -- otherwise you'll get an error resulting from the blank migration files.
+
+# what are we trying to do
+# create a table called costumes
+# id always included with ActiveRecord
+
+class CreateCostumes < ActiveRecord::Migration[5.2]
+    #method called change first
+    def change
+        # combines up and down
+        # what are we doing...name of block...t stands for table...t.datatype 
+        # name of our columns
+        create_table :costumes do |t|
+            t.string :name
+            t.string :size
+            t.string :image_url
+            t.float :price
+            #created at timestamp and updated at timestamp
+            t.timestamps
+        end
+    end
+
+    # # will just use change^^
+    # def up
+    #     # method to be run when rake db:rollback is executed
+    # end
+    # def down
+    #     # method to be run when rake db:migrate is executed
+    # end
+end
+
